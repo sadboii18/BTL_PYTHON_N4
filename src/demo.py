f86db76d6258c486dca812a32521e82fee7ce031
sadboii18 <dominhduc25013615@gmail.com>
2024-10-30 21:04:03 +0700

    update

--- src/demo.py
+++ src/demo.py
@@ -2,38 +2,135 @@ import pygame
 
 pygame.init()
 
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Shooter')
 
+#set framerate
+clock = pygame.time.Clock()
+FPS = 60
+
+#define player action variables
+
+moving_left_player1 = False
+moving_right_player1 = False
+
+moving_left_player2 = False
+moving_right_player2 = False
+
+
+#define colours
+BG = (144, 201, 120)
+
+def draw_bg():
+    screen.fill(BG)
+
+
 
 class Soldier(pygame.sprite.Sprite):
-    def __init__(self, x, y, scale):
+    def __init__(self, char_type, x, y, scale, speed):
         pygame.sprite.Sprite.__init__(self)
-        img = pygame.image.load('../assets/img/player/Idle/0.png')
+        self.char_type = char_type
+        self.speed = speed
+        self.direction = 1
+        self.flip = False
+        img = pygame.image.load(f'../assets/img/{self.char_type}/Idle/0.png')
         self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
-    def draw(self):
-        screen.blit(self.image, self.rect)
 
-player = Soldier(200, 200, 3)
-player2 = Soldier(400, 200, 3)
+    def move(self, moving_left, moving_right):
+        #reset movement variables
+        dx = 0
+        dy = 0
+
+        #assign movement variables if moving left or right
+        if moving_left:
+            dx = -self.speed
+            self.flip = True
+            self.direction = -1
+        if moving_right:
+            dx = self.speed
+            self.flip = False
+            self.direction = 1
+
+
+        #update rectangle position
+        self.rect.x += dx
+        self.rect.y += dy
+
+
+    def draw(self):
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
+
+
+
+player = Soldier('player', 200, 200, 3, 5)
+enemy = Soldier('enemy', 400, 200, 3, 5)
+
 
 
 run = True
 while run:
 
+    clock.tick(FPS)
+
+    draw_bg()
+
     player.draw()
-    player2.draw()
+    enemy.draw()
+
+    enemy.move(moving_left_player2, moving_right_player2)
+    player.move(moving_left_player1, moving_right_player1)
 
     for event in pygame.event.get():
-        # quit game
+        #quit game
         if event.type == pygame.QUIT:
             run = False
+
+        #Player1
+        #Khi giư phím
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                moving_left_player1 = True
+            if event.key == pygame.K_d:
+                moving_right_player1 = True
+            if event.key == pygame.K_ESCAPE:
+                run = False
+
+
+        #Khi nhả phím
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                moving_left_player1 = False
+            if event.key == pygame.K_d:
+                moving_right_player1 = False
+
+        # Player2
+        # Khi giư phím
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                moving_left_player2 = True
+            if event.key == pygame.K_RIGHT:
+                moving_right_player2 = True
+            if event.key == pygame.K_ESCAPE:
+                run = False
+
+        # Khi nhả phím
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                moving_left_player2 = False
+            if event.key == pygame.K_RIGHT:
+                moving_right_player2 = False
+
+
+
+
+
 
     pygame.display.update()
 
